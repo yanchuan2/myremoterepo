cc235b352f14a06d57c4561b25be72c86b53a4c2
HXW <1635892094@qq.com>
2025-10-26 10:29:16 +0800

    test: Pass move command test cases

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -1,82 +1,80 @@
 #include <gtest/gtest.h>
+
 #include <memory>
 #include <tuple>
+
 #include "Executor.hpp"
 namespace adas
 {
-    bool operator==(const Pose& lhs, const Pose& rhs)
-    {  // 为Pose定义==运算符重载，使得在测试中比较Pose对象变得简单明了， std::tie提高代码可读性
-        return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
-    }
-    //       测试套            测试用例名，清晰地描述了预期行为和测试的条件，更容易理解测试在验证什么
-    TEST(ExecutorTest, should_return_init_pose_when_without_command)
-    {
-             // given
+bool operator==(const Pose& lhs, const Pose& rhs)
+{  // 为Pose定义==运算符重载，使得在测试中比较Pose对象变得简单明了， std::tie提高代码可读性
+    return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
+}
+//       测试套            测试用例名，清晰地描述了预期行为和测试的条件，更容易理解测试在验证什么
+TEST(ExecutorTest, should_return_init_pose_when_without_command)
+{
+    // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
-             // when		测试遵循了 Arrange-Act-Assert (AAA) 模式，这是提高可读性和维护性的好习惯
-             // then
+    // when		测试遵循了 Arrange-Act-Assert (AAA) 模式，这是提高可读性和维护性的好习惯
+    // then
     const Pose target({0, 0, 'E'});
     ASSERT_EQ(target, executor->Query());
-    }
-    TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
-    {
-             // given
+}
+TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
+{
+    // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor());
-             // then
+    // then
     const Pose target({0, 0, 'N'});
     ASSERT_EQ(target, executor->Query());
-    }
-    // M
-    TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
-    {
-        // given
+}
+// M
+TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
+{
+    // given
     std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
 
-         // when
+    // when
     executor->Execute("M");
 
-            // then
+    // then
     const Pose target({1, 0, 'E'});
     ASSERT_EQ(target, executor->Query());
-    }
-    TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
-    {
-        // given
-        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+}
+TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
 
-        // when
-        executor->Execute("L");
-        executor->Execute("L");
-        executor->Execute("M");
+    // when
+    executor->Execute("LLM");//初始朝南 左转两次 再向前一步
 
-        // then
-        const Pose target({-1, 0, 'W'});
-        ASSERT_EQ(target, executor->Query());
-    }
-    TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
-    {
-        // given
-        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // then
+    const Pose target({-1, 0, 'W'});
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
 
-        // when
-        executor->Execute("L");
-        executor->Execute("M");
+    // when
+    executor->Execute("LM");//初始朝南 左转一次 向前一步
 
-        // then
-        const Pose target({0, 1, 'W'});
-        ASSERT_EQ(target, executor->Query());
-    }
-    TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
-    {
-        // given
-        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // then
+    const Pose target({0, 1, 'N'});
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
 
-        // when
-        executor->Execute("R");
-        executor->Execute("M");
+    // when
+    executor->Execute("RM");//初始朝南 右转一次 向前一次
 
-        // then
-        const Pose target({0, -1, 'S'});
-        ASSERT_EQ(target, executor->Query());
-    }
-}// namespace adas
+    // then
+    const Pose target({0, -1, 'S'});
+    ASSERT_EQ(target, executor->Query());
+}
+}  // namespace adas
