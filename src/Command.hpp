3234c694d69552d0cbafa80115fa0986f7504429
HXW <1635892094@qq.com>
2025-11-14 17:54:34 +0800

    Funtional Programming

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,53 +1,79 @@
 #pragma once 
 #include "ExecutorImpl.hpp"
 #include "PoseHandler.hpp"
+#include <functional>
 namespace adas
 {
-    class ICommand
+    //class ICommand
+    //{
+    //public:
+    //    virtual ~ICommand() = default;
+    //    virtual void DoOperate(PoseHandler& poseHandler) const noexcept = 0;
+    //};
+    class MoveCommand final//: public ICommand
     {
     public:
-        virtual ~ICommand() = default;
-        virtual void DoOperate(PoseHandler& poseHandler) const noexcept = 0;
-    };
-    class MoveCommand final : public ICommand
-    {
-    public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
-            if (poseHandler.IsFast()) {
+    //    void DoOperate(PoseHandler& poseHandler) const noexcept override
+    //    {
+    //        if (poseHandler.IsFast()) {
+    //            poseHandler.Move();
+    //        }
+    //        poseHandler.Move();
+    //    }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
+            if (poseHandler.IsFast())
+            {
                 poseHandler.Move();
             }
+
             poseHandler.Move();
-        }
+        };
     };
-    class TurnLeftCommand final : public ICommand
+    class TurnLeftCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
+    //    void DoOperate(PoseHandler& poseHandler) const noexcept override
+    //    {
+    //        if (poseHandler.IsFast()) {
+    //            poseHandler.Move();
+    //        }
+    //        poseHandler.TurnLeft();
+    //    }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
             if (poseHandler.IsFast()) {
                 poseHandler.Move();
             }
+
             poseHandler.TurnLeft();
-        }
+        };
     };
-    class TurnRightCommand final : public ICommand
+    class TurnRightCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
+    //    void DoOperate(PoseHandler& poseHandler) const noexcept override
+    //    {
+    //        if (poseHandler.IsFast()) {
+    //            poseHandler.Move();
+    //        }
+    //        poseHandler.TurnRight();
+    //    }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
             if (poseHandler.IsFast()) {
                 poseHandler.Move();
             }
+
             poseHandler.TurnRight();
-        }
+        };
     };
-    class FastCommand final : public ICommand
+    class FastCommand final //: public ICommand
     {
     public:
-        void DoOperate(PoseHandler& poseHandler) const noexcept override
-        {
+    //    void DoOperate(PoseHandler& poseHandler) const noexcept override
+    //    {
+    //        poseHandler.Fast();
+    //    }
+        const std::function<void(PoseHandler& poseHandler)> operate = [](PoseHandler& poseHandler) noexcept {
             poseHandler.Fast();
-        }
+        };
     };
 }
--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,9 +1,9 @@
 #include "ExecutorImpl.hpp"
 
 #include <memory>
+#include <unordered_map>
 
 #include "Command.hpp"
-#include <unordered_map>
 namespace adas
 {
 Executor* Executor::NewExecutor(const Pose& pose) noexcept
@@ -15,19 +15,28 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
-    cmderMap.emplace('M', std::make_unique<MoveCommand>());
-    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
-    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
-    cmderMap.emplace('F', std::make_unique<FastCommand>());
+    std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
+    MoveCommand moveCommand;
+    cmderMap.emplace('M', moveCommand.operate);
 
-    for (const auto cmd : commands) {
-        const auto it=cmderMap.find(cmd);
+    TurnLeftCommand turnLeftCommand;
+    cmderMap.emplace('L', turnLeftCommand.operate);
 
-        if (it != cmderMap.end()) {
-            it->second->DoOperate(poseHandler);  // 最外层的函数是ExecutorImpl类型的指针调用的，所以*this是executor
+    TurnRightCommand turnRightCommand;
+    cmderMap.emplace('R', turnRightCommand.operate);
+
+    FastCommand fastCommand;
+    cmderMap.emplace('F', fastCommand.operate);
+
+    for (const auto cmd : commands) 
+    {
+        const auto it = cmderMap.find(cmd);
+        if (it != cmderMap.end())
+        {
+            it->second(poseHandler);
         }
     }
+    
     return;
 }
 void ExecutorImpl::Move() noexcept
