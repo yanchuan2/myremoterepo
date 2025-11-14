72963999d25049031e52c43f4aa242bfdc0e3b22
HXW <1635892094@qq.com>
2025-11-14 10:05:34 +0800

    Revise by std:map

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,6 +1,9 @@
 #include "ExecutorImpl.hpp"
+
 #include <memory>
+
 #include "Command.hpp"
+#include <unordered_map>
 namespace adas
 {
 Executor* Executor::NewExecutor(const Pose& pose) noexcept
@@ -12,30 +15,17 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    for (int i = 0; i < (int)commands.size(); i++) {
-        std::unique_ptr<ICommand> cmder;
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
 
-        if(commands[i]=='M')
-        {
-            cmder=std::make_unique<MoveCommand>();
-        }
-        else if(commands[i]=='L')
-        {
-            cmder=std::make_unique<TurnLeftCommand>();
-        }
-        else if(commands[i]=='R')
-        {
-            cmder=std::make_unique<TurnRightCommand>();
-        }
-        else if(commands[i]=='F')
-        {
-            cmder = std::make_unique<FastCommand>();
-        }
+    for (const auto cmd : commands) {
+        const auto it=cmderMap.find(cmd);
 
-
-        if(cmder)
-        {
-            cmder->DoOperate(poseHandler);//最外层的函数是ExecutorImpl类型的指针调用的，所以*this是executor
+        if (it != cmderMap.end()) {
+            it->second->DoOperate(poseHandler);  // 最外层的函数是ExecutorImpl类型的指针调用的，所以*this是executor
         }
     }
     return;
@@ -54,15 +44,15 @@ void ExecutorImpl::Move() noexcept
 }
 void ExecutorImpl::TurnLeft() noexcept
 {
-        if (pose.heading == 'N') {
-            pose.heading = 'W';
-        } else if (pose.heading == 'E') {
-            pose.heading = 'N';
-        } else if (pose.heading == 'S') {
-            pose.heading = 'E';
-        } else if (pose.heading == 'W') {
-            pose.heading = 'S';
-        }
+    if (pose.heading == 'N') {
+        pose.heading = 'W';
+    } else if (pose.heading == 'E') {
+        pose.heading = 'N';
+    } else if (pose.heading == 'S') {
+        pose.heading = 'E';
+    } else if (pose.heading == 'W') {
+        pose.heading = 'S';
+    }
 }
 void ExecutorImpl::TurnRight() noexcept
 {
@@ -78,7 +68,7 @@ void ExecutorImpl::TurnRight() noexcept
 }
 void ExecutorImpl::Fast() noexcept
 {
-    fast=!fast;
+    fast = !fast;
 }
 bool ExecutorImpl::IsFast() const noexcept
 {
