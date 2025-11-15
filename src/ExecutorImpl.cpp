7fd3023442d79ee989b710c99669a3f2395a217d
HXW <1635892094@qq.com>
2025-11-15 10:31:21 +0800

    Semantic intialization

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -15,13 +15,20 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
-    cmderMap.emplace('M', MoveCommand());
-    cmderMap.emplace('L', TurnLeftCommand());
-    cmderMap.emplace('R', TurnRightCommand());
-    cmderMap.emplace('F', FastCommand());
+    //std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
+    //cmderMap.emplace('M', MoveCommand());
+    //cmderMap.emplace('L', TurnLeftCommand());
+    //cmderMap.emplace('R', TurnRightCommand());
+    //cmderMap.emplace('F', FastCommand());
+    const std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap
+    {
+        {'M', MoveCommand()}, 
+        {'L', TurnLeftCommand()}, 
+        {'R', TurnRightCommand()}, 
+        {'F', FastCommand()},
+    };
 
-    for (const auto cmd : commands) 
+        for (const auto cmd : commands)
     {
         const auto it = cmderMap.find(cmd);
         if (it != cmderMap.end())
